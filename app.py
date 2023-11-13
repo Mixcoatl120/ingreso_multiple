5084a9e22ad20ae881846e6e9033b07ab702f11b
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2023-11-13 12:29:49 -0600

    13/11/2023 #2

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask,render_template
+﻿from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Asea2023@localhost/siset'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Asea2023@localhost/users'
+app.config['SECRET_KEY'] = 'MIXCOATL12O'
 db.init_app(app)
 
 @app.route('/')
 def index():
+    
     return render_template('index.html')
+
+@app.route('/agregar_usuario', methods=['POST','GET'])
+def agregar_usuario():
+    nombre = request.form.get('nombre')
+    email = request.form.get('email')
+    rs = request.form.get('rs')
+    usuarios = Usuario.query.all()
+    print(rs)
+
+    nuevo_usuario = Usuario(nombre=nombre, email=email)
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+
+    return render_template('index.html',usuarios=usuarios)
 
 if __name__ == '__main__':
     app.run()
